984c3656e35400615013635437673e84cb4a4bfc
WarKB <warkb@yandex.ru>
2018-05-16 12:07:12 +0400

    start git

---
+++ mainpage/views.py
@@ -0,0 +1,7 @@
+from django.template import RequestContext, loader
+from django.http import HttpResponse
+
+# Create your views here.
+def index(request):
+    template = loader.get_template('mainpage\\authpage.html')
+    return HttpResponse(template.render())
