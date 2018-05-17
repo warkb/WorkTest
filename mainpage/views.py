7210cb790865a71c2695947b835ac642bfae0b99
WarKB <warkb@yandex.ru>
2018-05-17 11:37:51 +0400

    change template dir

--- mainpage/views.py
+++ mainpage/views.py
@@ -3,5 +3,5 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    template = loader.get_template('mainpage\\authpage.html')
+    template = loader.get_template('mainpage/authpage.html')
     return HttpResponse(template.render())
