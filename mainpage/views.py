ca13998fbd88150929b4ed6e9ff3bde8826a37c1
Vladimir <warkb@yandex.ru>
2018-05-21 09:28:49 +0000

    Теперь Django получает токен

--- mainpage/views.py
+++ mainpage/views.py
@@ -1,5 +1,6 @@
 import json
 import urllib
+import requests
 from django.template import RequestContext, loader
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import redirect
@@ -7,7 +8,7 @@ from django.shortcuts import redirect
 app_id = '6481880'
 app_secret = 'VgrTmwquRc6yaCCPiHc1'
 
-token_request = 'https://oauth.vk.com/access_token?client_id=6481880&client_secret=VgrTmwquRc6yaCCPiHc1&redirect_uri=http://mysite.ru&code={code}'
+token_request = 'https://oauth.vk.com/access_token?client_id=6481880&client_secret=VgrTmwquRc6yaCCPiHc1&redirect_uri=http://warkb.pythonanywhere.com/makeuser&code={code}'
 profile_request = 'https://api.vk.com/method/users.get?user_ids={user_id}&v=5.75'
 friends_request = 'https://api.vk.com/method/friends.get?user_id={user_id}&order=name&count=5&offset=2&fields=name&name_case=nom&v=5.75'
 
@@ -29,8 +30,8 @@ def index(request):
 
 def makeuser(request):
     code = request.GET.get('code')
-    response = urllib.request.urlopen(token_request.format(code=code))
-    json_response = json.loads(response.read().decode())
+    response = requests.get(token_request.format(code=code))
+    json_response = response.json()
     return HttpResponse(str(json_response))
 
 def userpage(request):
@@ -39,12 +40,12 @@ def userpage(request):
     profileDict = sendRequestToApiVK(profile_request.format(user_id=user_id),
         access_token)[0]
     username = profileDict[0]['first_name'] + ' ' + profileDict[0]['last_name']
-    friendsDict = sendRequestToApiVK(friends_request.format(user_id=user_id), 
+    friendsDict = sendRequestToApiVK(friends_request.format(user_id=user_id),
         access_token)
-    friendsCSV = ','.join([friend['last_name'] + ' ' + 
+    friendsCSV = ','.join([friend['last_name'] + ' ' +
         friend['first_name'] for friend in x])
 
-    return HttpResponse('user_id: %s<br>token: %s<br>username: %s<br>friends: %s<br>' % (user_id, 
+    return HttpResponse('user_id: %s<br>token: %s<br>username: %s<br>friends: %s<br>' % (user_id,
         access_token, username, friendsCSV))
 
 def clearRequest(request):
@@ -54,7 +55,7 @@ def clearRequest(request):
 
 # Запрос на друзей по id
 
-# https://api.vk.com/method/friends.get?user_id=263728812&order=name&count=5&offset=2&fields=name&name_case=nom&v=5.75&access_token=8c64f1b2ee1896c56b6e96bc61932957a24091ee0833cffa8b9681589e6486100fcbb29896995fe542dce&v=V 
+# https://api.vk.com/method/friends.get?user_id=263728812&order=name&count=5&offset=2&fields=name&name_case=nom&v=5.75&access_token=8c64f1b2ee1896c56b6e96bc61932957a24091ee0833cffa8b9681589e6486100fcbb29896995fe542dce&v=V
 
 # getProf
 # https://api.vk.com/method/users.get?user_ids=263728812&v=5.75&access_token=8c64f1b2ee1896c56b6e96bc61932957a24091ee0833cffa8b9681589e6486100fcbb29896995fe542dce&v=V
